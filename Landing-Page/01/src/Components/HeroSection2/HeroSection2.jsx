b2fb39fc9986b4105aaf68a1c5a2ee8e048b162e
AmaanAli8882 <amaanali8882@gmail.com>
2024-07-31 21:33:40 +0530

    Add files via upload

---
+++ Landing-Page/01/src/Components/HeroSection2/HeroSection2.jsx
@@ -0,0 +1,102 @@
+// import { SiApollographql, SiNotion } from 'react-icons/si'
+import "./HeroSection2.css";
+import { FaAddressCard } from "react-icons/fa6";
+
+const HeroSection2 = () => {
+  return (
+    <>
+      <section className=" p-10">
+        <div className="max-w-6xl mx-auto">
+          <h1 className="text-2xl font-bold text-center mb-12">
+            Trusted by customer-led product companies
+          </h1>
+          <div className="flex flex-wrap -mx-4 ">
+            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
+              <div className="bg-transparent p-6 rounded-lg shadow-xl text-center">
+                <FaAddressCard className="text-4xl mb-4 mx-auto text-[#4a90e2]" />
+                <h3 className="text-xl font-semibold mb-2">Canva</h3>
+                <p className="text-neutral-500">
+                  Description for service 1. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
+              <div className="bg-transparent p-6 rounded-lg shadow-lg text-center">
+                <FaAddressCard className="text-4xl mb-4 mx-auto text-[#4a90e2]" />
+                <h3 className="text-xl font-semibold mb-2">Descript</h3>
+                <p className="text-neutral-500">
+                  Description for service 2. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
+              <div className="bg-transparent p-6 rounded-lg shadow-lg text-center">
+                <FaAddressCard className="text-4xl mb-4 mx-auto text-[#4a90e2]" />
+                <h3 className="text-xl font-semibold mb-2">Apollo</h3>
+                <p className="text-neutral-500">
+                  Description for service 3. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
+              <div className="bg-transparent p-6 rounded-lg shadow-lg text-center">
+                <FaAddressCard className="text-4xl mb-4 mx-auto text-[#4a90e2]" />
+                <h3 className="text-xl font-semibold mb-2">Strava</h3>
+                <p className="text-neutral-500">
+                  Description for service 4. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
+              <div className="bg-transparent p-6 rounded-lg shadow-lg text-center">
+                <FaAddressCard className="text-4xl mb-4 mx-auto text-[#4a90e2]" />
+                <h3 className="text-xl font-semibold mb-2">Boll & Branch</h3>
+                <p className="text-neutral-500">
+                  Description for service 5. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
+              <div className="bg-transparent p-6 rounded-lg shadow-lg text-center">
+                <FaAddressCard className="text-4xl mb-4 mx-auto text-[#4a90e2]" />
+                <h3 className="text-xl font-semibold mb-2">Atlassian</h3>
+                <p className="text-neutral-500">
+                  Description for service 6. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
+              <div className="bg-transparent p-6 rounded-lg shadow-lg text-center">
+                <FaAddressCard className="text-4xl mb-4 mx-auto text-[#4a90e2]" />
+                <h3 className="text-xl font-semibold mb-2">Arc</h3>
+                <p className="text-neutral-500">
+                  Description for service 7. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
+              <div className="bg-transparent p-6 rounded-lg shadow-lg text-center">
+                <FaAddressCard className="text-4xl mb-4 mx-auto text-[#4a90e2]" />
+                <h3 className="text-xl font-semibold mb-2">Coda</h3>
+                <p className="text-neutral-500">
+                  Description for service 8. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr className="mx-20 opacity-10 my-12" />
+    </>
+  );
+};
+
+export default HeroSection2;
