91b0ada7f79461b09eec970df3ea54edc41b9a00
AmaanAli8882 <amaanali8882@gmail.com>
2024-07-31 21:38:13 +0530

    Add files via upload

---
+++ Landing-Page/01/src/App.jsx
@@ -0,0 +1,23 @@
+import Footer from "./Components/Footer/Footer"
+import HeroSection from "./Components/HeroSection/HeroSection"
+import HeroSection2 from "./Components/HeroSection2/HeroSection2"
+import HeroSection3 from "./Components/HeroSection3/HeroSection3"
+import HeroSection4 from "./Components/HeroSection4/HeroSection4"
+import Navbar from "./Components/Navbar/Navbar"
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <HeroSection/>
+      <HeroSection2/>
+      <HeroSection3/>
+      <HeroSection4/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
