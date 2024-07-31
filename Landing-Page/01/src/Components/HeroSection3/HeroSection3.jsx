ea4724317d32f659dcac06a6f38ccef06c5d6c75
AmaanAli8882 <amaanali8882@gmail.com>
2024-07-31 21:35:01 +0530

    Add files via upload

---
+++ Landing-Page/01/src/Components/HeroSection3/HeroSection3.jsx
@@ -0,0 +1,19 @@
+
+
+
+const HeroSection3 = () => {
+  return (
+    
+    <>
+    <div>
+        <div>
+            <h1 className="md:text-xl mt-20 md:ms-80 text-[#914ac8] sm:text-[3vw] sm:ms-32 sm:pb-4">The Enterpret Advantage</h1>
+            <p className="md:mx-72 md:ps-10 md:text-2xl md:mt-5 sm:text-md sm:ps-10 sm:mx-24">Enterpret automatically unifies your customer feedback across all data types and builds adaptive AI models tailored to your feedback architecture, delivering precise and granular insights.</p>
+        </div>
+        <hr className="mx-20 opacity-10 my-12" />
+    </div>
+    </>
+  ) 
+}
+
+export default HeroSection3
